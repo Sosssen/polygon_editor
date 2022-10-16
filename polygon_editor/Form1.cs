f929e7d42a5338b1b3fc5d3af4c126112ffd0d2d
Sosssen <jakub.sosnowski2001@gmail.com>
2022-10-17 00:13:48 +0200

    Add highlighting a polygon when mouse hovers over it

--- polygon_editor/Form1.cs
+++ polygon_editor/Form1.cs
@@ -33,6 +33,9 @@ namespace polygon_editor
         private bool colorEdge = false;
         private Point edgeToColor = new Point();
 
+        private bool colorPolygon = false;
+        private List<Point> polygonToColor = null;
+
         // 0 - nothing, 1 - point, 2 - edge, 3 - polygon
         private int moving = 0;
         private (int, int) pointToMove;
@@ -40,6 +43,7 @@ namespace polygon_editor
         private Point startingPoint = new Point();
         private Point startingPointA = new Point();
         private Point startingPointB = new Point();
+        private List<Point> polygonToMove = null;
         public polygon_editor()
         {
             InitializeComponent();
@@ -128,6 +132,10 @@ namespace polygon_editor
                             
                             // Debug.WriteLine($"P1: {pointToMove1.Item1}, {pointToMove1.Item2}, P2: {pointToMove2.Item1}, {pointToMove2.Item2}");
                         }
+                        else
+                        {
+                            var result3 = FindPolygon(e.X, e.Y);
+                        }
                     }
                 }
                 else if (e.Button == MouseButtons.Right)
@@ -202,6 +210,17 @@ namespace polygon_editor
                             {
                                 g.DrawLine(pen, polygon[i], polygon[(i + 1) % polygon.Count]);
                             }
+                        }
+                    }
+                    else if (colorPolygon)
+                    {
+                        if (polygon == polygonToColor)
+                        {
+                            g.DrawPolygon(redPen, arr);
+                        }
+                        else
+                        {
+                            g.DrawPolygon(pen, arr);
                         }
                     }
                     else
@@ -293,8 +312,9 @@ namespace polygon_editor
                     var result = FindPointInPolygons(e.X, e.Y);
                     if (result.Item1)
                     {
-                        colorEdge = false;
                         colorPoint = true;
+                        colorEdge = false;
+                        colorPolygon = false;
                         pointToColor = result.Item3;
                     }
                     else
@@ -304,12 +324,25 @@ namespace polygon_editor
                         {
                             colorPoint = false;
                             colorEdge = true;
+                            colorPolygon = false;
                             edgeToColor = result2.Item3;
                         }
                         else
                         {
-                            colorPoint = false;
-                            colorEdge = false;
+                            var result3 = FindPolygon(e.X, e.Y);
+                            if(result3.Item1)
+                            {
+                                colorPoint = false;
+                                colorEdge = false;
+                                colorPolygon = true;
+                                polygonToColor = result3.Item2;
+                            }
+                            else
+                            {
+                                colorPoint = false;
+                                colorEdge = false;
+                                colorPolygon = false;
+                            }
                         }
                     }
                 }
@@ -367,6 +400,83 @@ namespace polygon_editor
                 }
             }
             return (false, null, new Point(), new Point());
+        }
+
+        private (bool, List<Point>) FindPolygon(int x ,int y)
+        {
+            int counter;
+            Debug.WriteLine("run findpolygon");
+            foreach (var polygon in polygons)
+            {
+                counter = 0;
+                for (int i = 0; i < polygon.Count; i++)
+                {
+                    Point A = polygon[i];
+                    Point B = polygon[(i + 1) % polygon.Count];
+                    if (areIntersecting(0, 0, x, y, A.X, A.Y, B.X, B.Y) == 1) counter++;
+                }
+                Debug.WriteLine($"Polygon number {polygons.IndexOf(polygon)}, counter: {counter}");
+                if (counter % 2 == 1) return (true, polygon);
+            }
+            return (false, null);
+        }
+
+        int areIntersecting(
+    float v1x1, float v1y1, float v1x2, float v1y2,
+    float v2x1, float v2y1, float v2x2, float v2y2
+)
+        {
+            float d1, d2;
+            float a1, a2, b1, b2, c1, c2;
+
+            // Convert vector 1 to a line (line 1) of infinite length.
+            // We want the line in linear equation standard form: A*x + B*y + C = 0
+            // See: http://en.wikipedia.org/wiki/Linear_equation
+            a1 = v1y2 - v1y1;
+            b1 = v1x1 - v1x2;
+            c1 = (v1x2 * v1y1) - (v1x1 * v1y2);
+
+            // Every point (x,y), that solves the equation above, is on the line,
+            // every point that does not solve it, is not. The equation will have a
+            // positive result if it is on one side of the line and a negative one 
+            // if is on the other side of it. We insert (x1,y1) and (x2,y2) of vector
+            // 2 into the equation above.
+            d1 = (a1 * v2x1) + (b1 * v2y1) + c1;
+            d2 = (a1 * v2x2) + (b1 * v2y2) + c1;
+
+            // If d1 and d2 both have the same sign, they are both on the same side
+            // of our line 1 and in that case no intersection is possible. Careful, 
+            // 0 is a special case, that's why we don't test ">=" and "<=", 
+            // but "<" and ">".
+            if (d1 > 0 && d2 > 0) return 0;
+            if (d1 < 0 && d2 < 0) return 0;
+
+            // The fact that vector 2 intersected the infinite line 1 above doesn't 
+            // mean it also intersects the vector 1. Vector 1 is only a subset of that
+            // infinite line 1, so it may have intersected that line before the vector
+            // started or after it ended. To know for sure, we have to repeat the
+            // the same test the other way round. We start by calculating the 
+            // infinite line 2 in linear equation standard form.
+            a2 = v2y2 - v2y1;
+            b2 = v2x1 - v2x2;
+            c2 = (v2x2 * v2y1) - (v2x1 * v2y2);
+
+            // Calculate d1 and d2 again, this time using points of vector 1.
+            d1 = (a2 * v1x1) + (b2 * v1y1) + c2;
+            d2 = (a2 * v1x2) + (b2 * v1y2) + c2;
+
+            // Again, if both have the same sign (and neither one is 0),
+            // no intersection is possible.
+            if (d1 > 0 && d2 > 0) return 0;
+            if (d1 < 0 && d2 < 0) return 0;
+
+            // If we get here, only two possibilities are left. Either the two
+            // vectors intersect in exactly one point or they are collinear, which
+            // means they intersect in any number of points from zero to infinite.
+            if ((a1 * b2) - (a2 * b1) == 0.0f) return 2;
+
+            // If they are not collinear, they must intersect in exactly one point.
+            return 1;
         }
     }
 }
